c9096d4478e237f66b5b8e9ab31c53a6d05fa898
jo-arciniegas <arciniegasj626@gmail.com>
2023-02-23 21:11:27 +0500

    initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+// path: ./ecosystem.config.js
+
+module.exports = {
+  apps: [
+    {
+      name: "strapi",
+      cwd: "/root/language-learning-strapi",
+      script: "npm",
+      args: "start",
+    },
+  ],
+};
